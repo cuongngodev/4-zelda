2083071fcb5efacaf390f9d804bf8c3777cb06a7
Cuong Ngo <133584912+cuongngodev@users.noreply.github.com>
2025-10-26 07:33:55 -0400

    Update PlayerLiftingState.js

--- src/zelda-5/src/states/entity/player/PlayerLiftingState.js
+++ src/zelda-5/src/states/entity/player/PlayerLiftingState.js
@@ -13,41 +13,48 @@ export default class PlayerLiftingState extends State {
         super();
         this.player = player;
         this.object = object;
-        
+        this.isLiftingComplete = false;
       
         this.animation = {
-			[Direction.Up]: new Animation([6,7,8], 0.1),
-			[Direction.Down]: new Animation([0,1,2], 0.1),
-			[Direction.Left]: new Animation([9,10,11], 0.1),
-			[Direction.Right]: new Animation([3,4,5], 0.1),
+			[Direction.Up]: new Animation([6,7,8], 0.1, 1),
+			[Direction.Down]: new Animation([0,1,2], 0.1, 1),
+			[Direction.Left]: new Animation([9,10,11], 0.1, 1),
+			[Direction.Right]: new Animation([3,4,5], 0.1, 1),
 		};
+        this.finishedLiftingAnimation= {
+            [Direction.Up]: new Animation([8], 1),
+            [Direction.Down]: new Animation([2], 1),
+            [Direction.Left]: new Animation([11], 1),
+            [Direction.Right]: new Animation([5], 1),
+        }
         
     }
 
-    enter() {
+    enter(pot) {
+        this.pot = pot;
+        this.isLiftingComplete = false;
         this.player.sprites = this.player.liftingSprites;
         this.player.currentAnimation = this.animation[this.player.direction];
+        
+        // Start the lifting animation on the pot
+        if (this.pot && this.pot.onLift) {
+            this.pot.onLift(this.player);
+        }
     }
 
-    update(dt) {
-        // this.player.sprites = this.player.liftingSprites[0];
-        this.checkForCarryingMovement();
+    update(dt) {        
+        // Check if lifting animation is complete
+        if (this.player.currentAnimation.isDone() && !this.isLiftingComplete) {
+            this.player.currentAnimation.refresh();
+            this.isLiftingComplete = true;
+            this.player.changeState(PlayerStateName.Carrying, this.pot);
+
+        }
         
     }
-    checkForCarryingMovement(){
-        if (input.isKeyPressed(Input.KEYS.S)) {
-			this.player.direction = Direction.Down;
-			this.player.changeState(PlayerStateName.Carrying);
-		} else if (input.isKeyPressed(Input.KEYS.D)) {
-			this.player.direction = Direction.Right;
-			this.player.changeState(PlayerStateName.Carrying);
-		} else if (input.isKeyPressed(Input.KEYS.W)) {
-			this.player.direction = Direction.Up;
-			this.player.changeState(PlayerStateName.Carrying);
-		} else if (input.isKeyPressed(Input.KEYS.A)) {
-			this.player.direction = Direction.Left;
-			this.player.changeState(PlayerStateName.Carrying);
-		}
+
+    exit() {
+      
     }
 
 }
