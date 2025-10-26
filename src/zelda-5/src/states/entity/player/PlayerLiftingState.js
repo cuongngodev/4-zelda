f0d06fe1b5ef0ac08dfbf1b5713368ef11c6fc5a
Cuong Ngo <133584912+cuongngodev@users.noreply.github.com>
2025-10-25 22:42:06 -0400

    Update PlayerLiftingAnimation, add movement.

--- src/zelda-5/src/states/entity/player/PlayerLiftingState.js
+++ src/zelda-5/src/states/entity/player/PlayerLiftingState.js
@@ -1,8 +1,9 @@
 import Animation from "../../../../lib/Animation.js";
+import Input from "../../../../lib/Input.js";
 import State from "../../../../lib/State.js";
 import Direction from "../../../enums/Direction.js";
 import PlayerStateName from "../../../enums/PlayerStateName.js";
-import { context, DEBUG } from "../../../globals.js";
+import { context, DEBUG, input } from "../../../globals.js";
 
 export default class PlayerLiftingState extends State {
     /**
@@ -15,22 +16,38 @@ export default class PlayerLiftingState extends State {
         
       
         this.animation = {
-			[Direction.Up]: new Animation([6], 1),
-			[Direction.Down]: new Animation([0], 1),
-			[Direction.Left]: new Animation([9], 1),
-			[Direction.Right]: new Animation([3], 1),
+			[Direction.Up]: new Animation([6,7,8], 0.1),
+			[Direction.Down]: new Animation([0,1,2], 0.1),
+			[Direction.Left]: new Animation([9,10,11], 0.1),
+			[Direction.Right]: new Animation([3,4,5], 0.1),
 		};
+        
     }
 
     enter() {
         this.player.sprites = this.player.liftingSprites;
         this.player.currentAnimation = this.animation[this.player.direction];
-        
-      
     }
 
     update(dt) {
-
+        // this.player.sprites = this.player.liftingSprites[0];
+        this.checkForCarryingMovement();
+        
+    }
+    checkForCarryingMovement(){
+        if (input.isKeyPressed(Input.KEYS.S)) {
+			this.player.direction = Direction.Down;
+			this.player.changeState(PlayerStateName.Carrying);
+		} else if (input.isKeyPressed(Input.KEYS.D)) {
+			this.player.direction = Direction.Right;
+			this.player.changeState(PlayerStateName.Carrying);
+		} else if (input.isKeyPressed(Input.KEYS.W)) {
+			this.player.direction = Direction.Up;
+			this.player.changeState(PlayerStateName.Carrying);
+		} else if (input.isKeyPressed(Input.KEYS.A)) {
+			this.player.direction = Direction.Left;
+			this.player.changeState(PlayerStateName.Carrying);
+		}
     }
 
 }
